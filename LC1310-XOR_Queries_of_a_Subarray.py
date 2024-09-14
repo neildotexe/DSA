019dd9613a4e0d4810aa899ff6e07e7da7d89f6d
neildotexe <rudranilgroy@gmail.com>
2024-09-14 12:08:40 +0530

    Update LC1310-XOR_Queries_of_a_Subarray.py

--- LC1310-XOR_Queries_of_a_Subarray.py
+++ LC1310-XOR_Queries_of_a_Subarray.py
@@ -1,3 +1,14 @@
+# Naive Brute Force Solution
+def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        answers = []
+        for left, right in queries:
+                xor_result = 0
+                for i in range(left, right+1):
+                        xor_result ^= arr[i]
+                answers.append(xor_result)
+        return answers
+
+# Optimized Solution using prefix xors
 def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
         prefix = [0]
         for n in arr:
